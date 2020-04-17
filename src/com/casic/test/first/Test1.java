f0e12bc8e1213bf87c9e3fe835f4763bb7f7bbfd
origin <https://github.com/wangynbo/origin.git>
2020-04-18 00:26:00 +0800

    第一次提交

---
+++ src/com/casic/test/first/Test1.java
@@ -0,0 +1,10 @@
+package com.casic.test.first;
+import java.util.*;
+public class Test1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        System.out.println(s);
+
+    }
+}
---
+++ src/com/casic/test/second/Test2.java
@@ -0,0 +1,5 @@
+package com.casic.test.second;
+public class Test2 {
+    public static void main(String[] args){
+    }
+}
